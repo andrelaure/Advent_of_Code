512b8c1c05453b9fdbdd14177602b95742db76ef
andrelaure <67538262+andrelaure@users.noreply.github.com>
2022-12-05 14:24:43 +0100

    Update 04_day.py

--- 2022/04_day.py
+++ 2022/04_day.py
@@ -58,8 +58,7 @@ with open("04_input.txt", "r") as file:
                 num.append(int(y))
         if (num[0]<=num[2] and num[1]>=num[3]) or (num[0]>=num[2] and num[1]<=num[3]):
             fully_contain = fully_contain + 1
-            overlap = overlap + 1
-            
+            overlap = overlap + 1            
         elif(num[0]<=num[2]):
             if(num[1]>=num[2]):
                 overlap = overlap + 1
@@ -71,19 +70,17 @@ with open("04_input.txt", "r") as file:
     print("The total assignemnt fully contain in other is: ", fully_contain)
     print("The total overlaps are: ", overlap)
 
-
-    '''--- Part Two ---
-    It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
-
-    In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
-
-    5-7,7-9 overlaps in a single section, 7.
-    2-8,3-7 overlaps all of the sections 3 through 7.
-    6-6,4-6 overlaps in a single section, 6.
-    2-6,4-8 overlaps in sections 4, 5, and 6.
-    So, in this example, the number of overlapping assignment pairs is 4.
-
-    In how many assignment pairs do the ranges overlap?'''
-
-
 file.close()
+
+'''--- Part Two ---
+It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+5-7,7-9 overlaps in a single section, 7.
+2-8,3-7 overlaps all of the sections 3 through 7.
+6-6,4-6 overlaps in a single section, 6.
+2-6,4-8 overlaps in sections 4, 5, and 6.
+So, in this example, the number of overlapping assignment pairs is 4.
+
+In how many assignment pairs do the ranges overlap?'''
