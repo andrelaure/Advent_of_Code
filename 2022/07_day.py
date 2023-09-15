679b0e2b67fcdc3ec605f77d6bf3516f64005e62
andrelaure <67538262+andrelaure@users.noreply.github.com>
2023-09-15 16:03:34 +0200

    .

--- 2022/07_day.py
+++ 2022/07_day.py
@@ -1,7 +1,36 @@
 with open ("07_input.txt", "r") as file:
-    line = file.readline()
-    line = line[:-1]
 
+    path = [["root", 0]]
+    cur_dir_size = 0
 
+    while True:
+        line = file.readline()
+        line = line[:-1]
+        if line == "":
+            break
+
+        line = line.split(" ")
+        for val in line:
+            if "dir" in line or "ls" in line:
+                break
+            if val != "$":
+                if "cd" in line:
+                    if ".." in line:
+                        
+                        print("come back to the parent, add ", cur_dir_size)
+                        
+                    else:
+                        path.append([line[2], 0])
+                        print("create a new son called ", line[2])
+                        break
+                else:
+                    print(path[-1])
+                    for val in path[-1]:
+                        val[1] += int(line[0])
+                    
+                    #cur_dir_size += int(line[0])
+            #print(line)
+
+    print(path)
 
 file.close()
