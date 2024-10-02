3eff39ea3daa83fcf94461fbae63fb11c8de6383
andrelaure <67538262+andrelaure@users.noreply.github.com>
2024-10-02 13:12:52 +0200

    .

--- 2023/Day04/Program.cs
+++ 2023/Day04/Program.cs
@@ -2,17 +2,18 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 class Day04{
 
     private static void Main(){
 
-        string file_path = "04_day.txt";
-        string[] lines = File.ReadAllLines(file_path);  
+        string file_path = "004_day.txt";
+        string[] lines = File.ReadAllLines(file_path); 
+        //int scratchcards = 0; 
         
-        double totalPoints = PartOne(lines);
-        //int scratchcards = 0;
-        
+        //double totalPoints = PartOne(lines);
+        double totalPoints = PartTwo(lines);
 
         Console.WriteLine($"totalPoints: {totalPoints}");
     }
@@ -25,7 +26,6 @@ class Day04{
             int count = 0;
             string[] card = line.Split(':');
             string[] num = card[1].Split('|');
-
             int[] winNum = num[0].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             int[] haveNum = num[1].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
@@ -33,13 +33,49 @@ class Day04{
             if (count > 0)
                 linePoints = Math.Pow(2, count-1);
 
-            /*foreach (int numero in haveNum){
-                Console.WriteLine(numero);
-            }*/
-
             totalPoints += linePoints;
         }
 
         return totalPoints;
+    }
+    
+    private static double PartTwo(string[] lines){
+        Dictionary<int, int> scratchcardCopies = new Dictionary<int, int>();
+
+        for (int i=0; i<lines.Length; i++){  //foreach (string line in lines){
+
+            int count = 0;
+            string[] card = lines[i].Split(':');
+            string[] num = card[1].Split('|');
+            int[] winNum = num[0].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] haveNum = num[1].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            count = winNum.Intersect(haveNum).Count();
+
+            if (!scratchcardCopies.ContainsKey(i)){
+                scratchcardCopies.Add(i, 0);
+            }
+            
+            for (int j=i; j<=i+count; j++){
+                
+                if (scratchcardCopies.ContainsKey(j)){
+                    scratchcardCopies[j] ++;
+                }
+                else{
+                    scratchcardCopies.Add(j, 1);
+                }
+
+                foreach (var dict in scratchcardCopies){
+                    Console.WriteLine($"dict: {dict} at round {j}");
+                }
+                
+            }
+            Console.WriteLine("----");
+        
+        }
+        
+        
+        int result = scratchcardCopies.Values.Sum();
+        return result;
     }
 }
---
+++ 2023/Day05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Day04{
+
+    private static void Main(){
+
+        string file_path = "005_day.txt";
+        string[] lines = File.ReadAllLines(file_path);
+
+        Match match = Regex.Match(lines[0], @"^seeds: ");
+
+        string[] seeds = match.Groups[2].Value.Split(" ");
+
+        Console.WriteLine(match.Groups[0].Value);
+
+        //seeds: (\d)+
+        //s2s: (\d \d \d)+
+        //s2f: (\d \d \d)+        
+        //f2w: (\d \d \d)+
+        //w2l: (\d \d \d)+
+        //l2t: (\d \d \d)+
+        //t2h: (\d \d \d)+
+        //h2l: (\d \d \d)+
+
+        /*foreach(string line in lines){
+            Console.WriteLine(line);
+        }*/
+    }
+}
